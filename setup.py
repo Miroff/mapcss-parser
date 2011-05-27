6cbec3743576eaac850346c596a7e10b266dfdda
Miroff <mr.miroff@gmail.com>
2011-05-27 16:51:54 +0700

    :q

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='mapcss_parser',
+      version='0.1',
+      author="Miroff",
+      author_email="mr.miroff@gmail.com",
+      url='https://github.com/Miroff/mapcss-parser',
+      license="2-clause BSD",
+      packages=['mapcss_parser']
+      )
